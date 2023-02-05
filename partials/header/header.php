c3868422d7d955f326575dba615e477219f5d61a
Thulani Matshoba <thulani90.m@gmail.com>
2023-02-05 20:17:01 +0200

    created off canvas menu

--- partials/header/header.php
+++ partials/header/header.php
@@ -5,7 +5,6 @@
         </div><!-- .site-branding -->
 
         <nav id="site-navigation" class="uk-navbar-right main-navigation">
-
             <button class="uk-navbar-toggle menu-toggle uk-hidden@s" uk-navbar-toggle-icon href="#" uk-toggle="target: #offcanvas-push"></button>
             <?php get_template_part('partials/menus/header', 'menu'); ?>
         </nav><!-- #site-navigation -->
